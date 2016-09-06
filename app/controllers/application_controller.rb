a83ebf7e1811c0c8d6108ac0360603874a651c7d
AUG <agabet@octo.com>
2016-09-06 14:48:16 +0200

    changing status color fucntion to component statuses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_filter :get_component_status
+  
+  def get_component_status
+    components_ko = Component.where(status: false)
+    if components_ko.any?
+  	  @component_status = "status-ko"
+    else
+  	  @component_status = "status-ok"
+    end
+  end
+
 end
